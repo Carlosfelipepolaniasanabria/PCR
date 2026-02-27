6c2fc48136fe809e7f2ec512ba9b4bc3f2b7bed7
Carlosfelipepolaniasanabria <carlosf.polanias@unac.edu.co>
2026-02-27 09:34:06 -0500

    Creo proyecto con Next y con Express todo junto para una facil importacion de todos los elementos. Añado un registro y un login y tambien creo pagina principal con el navbar

---
+++ backend-pcr/models/user.model.js
@@ -0,0 +1,68 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+const bcrypt = require("bcrypt");
+
+const Usuario = sequelize.define("Usuario", {
+  correo: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  nombres: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellidos: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  celular: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  departamento: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  municipio: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  direccion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  barrio: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  estrato: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  tipo_documento: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  numero_documento: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  contrasena: {   // 🔥 NUEVO CAMPO
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  rol: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "usuario",
+  },
+}, {
+  tableName: "usuarios",
+  timestamps: true,
+});
+
+module.exports = Usuario;
